500e8e6f0505d0aac69bed359d6f2153149ac57e
Artem <xotabggg@gmail.com>
2024-06-08 23:52:32 +0700

    User-registration-version1

--- src/User.java
+++ src/User.java
@@ -1,13 +1,29 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class User {
     private String name;
     private String job;
     private String address;
     private int age;
+    private final Set<String> VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
+    private final Set<String> VALID_ADDRESSES = new HashSet(Arrays.asList("London", "New York", "Amsterdam"));
+
+    public User(String name, String job, String address, int age) {
+        if (name != null && VALID_JOBS.contains(job) && VALID_ADDRESSES.contains(address) && age >= 18) {
+            this.name = name;
+            this.job = job;
+            this.address = address;
+            this.age = age;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
 
     public int getAge() {
         return age;

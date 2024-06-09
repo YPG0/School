560d2ff4962f33769ee8450abfb129c0321dc1cb
Artem <xotabggg@gmail.com>
2024-06-09 13:17:25 +0700

    GameOfThrone-version1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        Map<String, House> westeros = new HashMap<>();
 
+        westeros.put("Stark", new House("Stark", "Wolf"));
+        westeros.put("Lanistar", new House("Lanistar", "lion"));
+        westeros.put("Barateon", new House("Barateon", "Deer"));
 
+        westeros.remove("Lanistar");
+
+        System.out.println(westeros.get("Barateon").getSigil());
+
+        for (Map.Entry<String,  House> x : westeros.entrySet())
+            System.out.println("Home name: " + x.getValue().getName() + " sigil: " + x.getValue().getSigil());
     }
 }

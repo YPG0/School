cf20f8386bf25619195ea11fe8f678168a098390
Artem <xotabggg@gmail.com>
2024-06-09 12:44:34 +0700

    revers-version1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] text = new int[] {1, 2, 3, 4, 5};
 
-
+        Example example = new Example();
+        System.out.println(Arrays.toString(example.revers(text)));
     }
 }

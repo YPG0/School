33c37e5b0184eca9de8d75bb5d2c09f23a16a18e
Artem <xotabggg@gmail.com>
2024-06-09 11:34:35 +0700

    Abstraction2-version1

---
+++ src/Character.java
@@ -0,0 +1,19 @@
+public abstract class Character {
+    private String name;
+    protected int power;
+    protected int sleight;
+    protected int intellect;
+    int health = 100;
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public Character(int power, int intellect, int sleight) {
+        this.power = power;
+        this.intellect = intellect;
+        this.sleight = sleight;
+    }
+
+    public void attack(Character character) {}
+}
